a92b2a5bd6786833eb86f945421a2394338f2af5
Chukwuebuka Ogbonna <ogbonnashedrach8@gmail.com>
2022-07-04 14:29:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let guess = [];
+let answer = document.getElementById("answer");
+let guessBox = document.querySelector("#guess");
+let lastGuess = document.getElementById("lastguess");
+let random = Math.floor(Math.random() * 10);
+let myAnswer = document.getElementById("submit");
+let restart = document.getElementById("restart");
+let incorrect = 10
+
+console.log(random);
+
+myAnswer.addEventListener("click", function myanswer() {
+  guess.push(guessBox.value);
+  submit();
+});
+
+function submit() {
+  lastGuess.textContent += guess.pop() + ', ';
+  if (isNaN(guessBox.value)){
+    answer.textContent = "Please input a number";
+    guess = [];
+    lastGuess.textContent = guess;
+  }
+  if (guessBox.value == random) {
+    answer.textContent = "You're right";
+  } else if (guessBox.value < random) {
+    answer.textContent = "Guess too low";
+  } else if (guessBox.value > random) {
+    answer.textContent = "Guess too high";
+  }
+  if(guessBox.value > incorrect){
+    answer.textContent = "Please follow the rules";
+    guess = []
+    lastGuess.textContent = guess
+  }
+
+  guessBox.value = " ";
+}
+restart.addEventListener("click", function restart() {
+  answer.textContent = " ";
+  guessBox.value = " ";
+  guess = [];
+  lastGuess.textContent ='Lastguess: ' + guess;
+});
